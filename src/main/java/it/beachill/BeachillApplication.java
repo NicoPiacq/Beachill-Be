49a977cc89a94c0ccfc8693ec628093c55e5896b
QuaiFederico <quaifede@gmail.com>
2023-12-11 18:48:20 +0100

    first commit

---
+++ src/main/java/it/beachill/BeachillApplication.java
@@ -0,0 +1,13 @@
+package it.beachill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/beachill/BeachillApplicationTests.java
@@ -0,0 +1,13 @@
+package it.beachill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
